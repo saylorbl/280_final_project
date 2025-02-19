812d5fe1d88da5d91702cbfb500dcc645e76b03b
saylorbl <143839558+saylorbl@users.noreply.github.com>
2025-02-19 11:49:31 -0500

    Most parking lots working (as far as labels go)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -21,11 +21,53 @@ rhit.buttonSetup = function () {
 		document.querySelector("#tripletsLabel").classList.add("invisible");
 	});
 
-	document.querySelector("#LakesideNorth").addEventListener("mouseover", () => {
-		document.querySelector("#LakesideNorthLabel").classList.remove("invisible");
+	document.querySelector("#lakesideNorth").addEventListener("mouseover", () => {
+		document.querySelector("#lakesideNorthLabel").classList.remove("invisible");
 	});
-	document.querySelector("#LakesideNorth").addEventListener("mouseout", () => {
-		document.querySelector("#LakesideNorthLabel").classList.add("invisible");
+	document.querySelector("#lakesideNorth").addEventListener("mouseout", () => {
+		document.querySelector("#lakesideNorthLabel").classList.add("invisible");
+	});
+
+	document.querySelector("#lakesideSouth").addEventListener("mouseover", () => {
+		document.querySelector("#lakesideSouthLabel").classList.remove("invisible");
+	});
+	document.querySelector("#lakesideSouth").addEventListener("mouseout", () => {
+		document.querySelector("#lakesideSouthLabel").classList.add("invisible");
+	});
+
+	document.querySelector("#percopo").addEventListener("mouseover", () => {
+		document.querySelector("#percopoLabel").classList.remove("invisible");
+	});
+	document.querySelector("#percopo").addEventListener("mouseout", () => {
+		document.querySelector("#percopoLabel").classList.add("invisible");
+	});
+
+	document.querySelector("#speed").addEventListener("mouseover", () => {
+		document.querySelector("#speedLabel").classList.remove("invisible");
+	});
+	document.querySelector("#speed").addEventListener("mouseout", () => {
+		document.querySelector("#speedLabel").classList.add("invisible");
+	});
+
+	document.querySelector("#hatfield").addEventListener("mouseover", () => {
+		document.querySelector("#hatfieldLabel").classList.remove("invisible");
+	});
+	document.querySelector("#hatfield").addEventListener("mouseout", () => {
+		document.querySelector("#hatfieldLabel").classList.add("invisible");
+	});
+
+	document.querySelector("#bic").addEventListener("mouseover", () => {
+		document.querySelector("#bicLabel").classList.remove("invisible");
+	});
+	document.querySelector("#bic").addEventListener("mouseout", () => {
+		document.querySelector("#bicLabel").classList.add("invisible");
+	});
+
+	document.querySelector("#commuter").addEventListener("mouseover", () => {
+		document.querySelector("#commuterLabel").classList.remove("invisible");
+	});
+	document.querySelector("#commuter").addEventListener("mouseout", () => {
+		document.querySelector("#commuterLabel").classList.add("invisible");
 	});
 };
 
